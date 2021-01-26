81653a47fb6c7f12a3c809ac518751fbdc2f32cd
Andres Benitez <abolnaits@hotmail.com>
2021-01-26 10:42:02 -0500

    App
    Basic style and html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class App extends React.Component{
+    ///Where the main app exists
+    render(){
+      return(
+        <div className="container main">
+          <h1 className="text-center">Todo list React App</h1>
+          <div id="task-container">
+            <div id="form-wrapper">
+            <form id="form">
+              <div className="form-group">
+                  <label for="title">Email address</label>
+                  <input type="text" className="form-control" id="title" aria-describedby="titleHelp"></input>
+                  <small id="titleHelp" className="form-text text-muted">The title of the task.</small>
+              </div>
+              
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            </div>
+            <div id="list-wrapper">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </div>
+          </div>
+        </div>
+      );   
+    }
 }
 
 export default App;
